82c1ca83cfac8d381bd2aefe686c3ac49d143a5e
Kristijan Baro <baro.kristijan@gmail.com>
2022-05-19 14:18:32 +0200

    health bars and timer

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ class Sprite {
         }
         this.color = color
         this.isAttacking
+        this.health = 100
     }
 
     draw() {
@@ -128,6 +129,36 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
     )
 }
 
+function determineWinner({ player, enemy, timerId }) {
+    clearTimeout(timerId)
+    document.querySelector('#displayText').style.display = 'flex'
+    if (player.health === enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Tie'           
+    } else if (player.health > enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 1 Wins'  
+    } else if (player.health < enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 2 Wins'  
+    }
+}
+
+
+let timer = 60
+let timerId
+function decreaseTimer() {
+    if(timer > 0) {
+        timerId = setTimeout(decreaseTimer, 1000)
+        timer--
+        document.querySelector('#timer').innerHTML = timer
+    }
+
+
+    if ( timer === 0) {
+        determineWinner({player, enemy, timerId })
+    }   
+}
+
+decreaseTimer()
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
@@ -161,7 +192,8 @@ function animate() {
         player.isAttacking
     ) {
         player.isAttacking = false
-        console.log ('go')
+        enemy.health -= 20
+        document.querySelector('#enemyHealth').style.width = enemy.health + '%'
     }
 
     if (
@@ -172,7 +204,13 @@ function animate() {
         enemy.isAttacking
     ) {
         enemy.isAttacking = false
-        console.log ('enemy attack sucessful')
+        player.health -= 20
+        document.querySelector('#playerHealth').style.width = player.health + '%'
+    }
+
+    //end game based on health
+    if (enemy.health <= 0 || player.health <= 0) {
+            determineWinner({player, enemy, timerId })
     }
 }
 
