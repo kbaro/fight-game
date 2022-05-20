5cd2e997802973e72531963c73717e08d39b421a
Kristijan Baro <baro.kristijan@gmail.com>
2022-05-20 03:28:28 +0200

    fixing a bug

--- js/classes.js
+++ js/classes.js
@@ -130,12 +130,24 @@ class Fighter extends Sprite {
         this.isAttacking = true
     }
 
+    takeHit(){
+        this.switchSprite('takeHit')
+        this.health -= 20
+    }
+
     switchSprite(sprite) {
-    if (
-        this.image === this.sprites.attack1.image && 
-        this.framesCurrent < this.sprites.attack1.framesMax -1 
+        // overriding all other animations with the attack animation
+        if (
+            this.image === this.sprites.attack1.image && 
+            this.framesCurrent < this.sprites.attack1.framesMax -1 
     ) 
-        return
+            return
+
+        // override when fighter gets hit
+        if (this.image === this.sprites.takeHit.image && 
+            this.framesCurrent < this.sprites.takeHit.framesMax -1 
+        )
+            return
 
        switch (sprite) {
             case 'idle':
@@ -176,9 +188,9 @@ class Fighter extends Sprite {
             }
                 break
             case 'takeHit':
-                if (this.image !== this.sprites.attack1.image) {
-                this.image = this.sprites.attack1.image
-                this.framesMax = this.sprites.attack1.framesMax
+                if (this.image !== this.sprites.takeHit.image) {
+                this.image = this.sprites.takeHit.image
+                this.framesMax = this.sprites.takeHit.framesMax
                 this.framesCurrent = 0
             }
                 break    
